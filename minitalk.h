1fb8a12b45c1bfe4bb57f88de41451410c384999
Furkan KIRMIZI <furkan_krmz@Furkan-Air.home>
2023-07-15 12:53:04 +0300

    minitalk.h

---
+++ minitalk.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: akirmizi <42istanbul.com.tr>               +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/11 17:18:01 by akirmizi          #+#    #+#             */
+/*   Updated: 2022/05/11 17:19:35 by akirmizi         ###   ########.tr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <signal.h>
+# include <unistd.h>
+# include "./ft_printf/ft_printf.h"
+# include <stdio.h>
+
+int	ft_atoi(const char *str);
+#endif
